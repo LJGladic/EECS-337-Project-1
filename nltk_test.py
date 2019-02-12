ef9752f0a98c479f2957c00d7fe2784a2ab807a2
acbosset <andrewbosset2019@u.northwestern.edu>
2019-02-11 22:54:25 -0600

    returns name of hosts but assumes cohosts

--- nltk_test.py
+++ nltk_test.py
@@ -1,7 +1,7 @@
 import nltk
 import json
 from nltk.corpus import stopwords
-
+import string
 # filter = "JSON file (*.json)|*.json|All Files (*.*)|*.*||"
 # filename = rs.OpenFileName("Open JSON File", filter)
 # data = json.load('gg2013.json')
@@ -10,19 +10,38 @@ from nltk.corpus import stopwords
 with open('gg2013.json') as f:
     data = json.load(f)
 
-tweet_stream = ''
-for tweet in data:
-    tweet_stream = tweet_stream + tweet["text"]
+
+# possible function for finding number of hosts, may just want to track in main loop
+def num_hosts():
+    tweet_stream = ''
+    for tweet in data:
+        tweet_stream = tweet_stream + tweet["text"]
 
 
-tokens = [t for t in tweet_stream.split()]
-freq = nltk.FreqDist(tokens)
-for key, val in freq.items():
+stop_words = (stopwords.words('english'))
+stop_words.extend(['golden', 'globe', 'globes', 'goldenglobes', 'rt'])
 
-    print (str(key) + ':' + str(val))
+hosts = []
+i = 0
+# could move lowercasing to entire text string but I think this will work better if we have to go tweet by tweet
+for t in data:
+    tweet = t["text"]
+    if tweet[:2] != "RT":
+        for ch in string.punctuation:
+            tweet = tweet.replace(ch, "")
 
-# stopwords.words('english')
-print(data[0]["text"])
+        tokens = [t.lower() for t in tweet.split() if t.lower() not in stop_words]
+        host_terms = ['host', 'hosts', 'cohost', 'cohosts',
+                      'co-host', 'hosting', 'co-hosts', 'coshosts']
+        # need faster method, might take too long
+        if any(t in host_terms for t in tokens):
+            i = i + 1
+            hosts.extend(nltk.bigrams(tokens))
 
-# tokens = [t for t in data["text"].split()]
-# freq = nltk.FreqDist(tokens)
+freq = nltk.FreqDist(hosts)
+# for key, val in freq.items():
+#     if val > 100:
+#         print (str(key) + ':' + str(val))
+
+print(sorted(freq, key=freq.get, reverse=True)[:2])
+print(i)
