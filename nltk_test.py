5bedf3c3600f82d6ac0ce0647458bba389fe4cc0
acbosset <andrewbosset2019@u.northwestern.edu>
2019-02-16 02:32:16 -0600

    more accurate award winner, need to clean answer

--- nltk_test.py
+++ nltk_test.py
@@ -14,13 +14,41 @@ with open('gg2013.json') as f:
 
 
 stop_words = (stopwords.words('english'))
-stop_words.extend(['golden', 'globe', 'globes', 'goldenglobes', 'year', 'next'])
+stop_words.extend(['golden', 'globe', 'globes', 'goldenglobes',
+                   'year', 'next', 'made', 'performance', 'role', 'miniseries'])
 
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama',
                         'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy',
                         'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
+
+condense_awards = ['cecil demille award',
+                   'best motion picture drama',
+                   'best performace actress drama',
+                   'best performace actor drama',
+                   'best motion picture comedy musical',
+                   'actress comedy musical performance',
+                   'actor comedy musical performace',
+                   'animated feature film',
+                   'foreign language film',
+                   'supporting actress best',
+                   'best supporting actor',
+                   'best director',
+                   'best screenplay',
+                   'best score',
+                   'best original song',
+                   'best TV series',
+                   'actress TV series',
+                   'actor TV series',
+                   'TV series comedy musical',
+                   'actress TV mucial comedy',
+                   'actor TV musical comedy',
+                   'TV mini series picture',
+                   'actress mini picture TV',
+                   'actor mini picture TV',
+                   'actress supporting TV series',
+                   'actor supporting TV series']
 
 
 def get_hosts(tweets):
@@ -83,23 +111,38 @@ def get_winner(year):
     else:
         official_awards = OFFICIAL_AWARDS_1819
 
+    # punctuation fucked shit up
     for award in official_awards:
+        for ch in string.punctuation:
+            award = award.replace(ch, "")
         award_tokens = [t.lower() for t in award.split() if t.lower() not in stop_words]
+        if 'television' in award_tokens:
+            award_tokens[award_tokens.index('television')] = 'tv'
+
+        if 'tv' in award_tokens:
+            if 'motion' in award_tokens:
+                award_tokens.remove('motion')
+            if 'picture' in award_tokens:
+                award_tokens.remove('picture')
+
+        print (award_tokens)
         possible_winners = {}
         # check to see if tweet has words in award name
         # remove award words and stop words
         for tweet in award_tweets:
             tweet_tokens = tweet["text"]
+
             combined_tokens = [value for value in award_tokens if value in tweet_tokens]
             percent = float(len(combined_tokens) / len(award_tokens))
-            if percent > .7:
+            #.7 with no punctuation
+            if percent > .9:
                 winner_name = [word for word in tweet_tokens if word not in award_tokens]
                 winner_name = " ".join(winner_name)
                 if winner_name not in possible_winners:
                     possible_winners[winner_name] = 1
                 else:
                     possible_winners[winner_name] += 1
-        winners[award] = sorted(possible_winners, key=possible_winners.get, reverse=True)[:1]
+        winners[award] = sorted(possible_winners, key=possible_winners.get, reverse=True)[: 1]
     # find tweets that contain certain percentage of award name,
     # remove stop words and award words,
     return winners
@@ -157,3 +200,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# ['tv', 'series', 'comedy', 'musical']
+# ['actress', 'tv', 'mucial', 'comedy']
+# ['actor', 'tv', 'musical', 'comedy']
+# ['tv', 'mini', 'series', 'picture']
+# ['actress', 'mini', 'picture', 'tv']
+# ['actor', 'mini', 'picture', 'tv']
+# ['actress', 'supporting', 'tv', 'series']
+# ['actor', 'supporting', 'tv', 'series']
