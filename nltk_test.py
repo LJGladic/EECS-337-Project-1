a8605500f505d55dbf6bcdc335b48da244414e70
acbosset <andrewbosset2019@u.northwestern.edu>
2019-02-16 15:43:14 -0600

    start of presenters

--- nltk_test.py
+++ nltk_test.py
@@ -138,13 +138,65 @@ def get_presenters(year):
     '''Presenters is a dictionary with the hard coded award
     names as keys, and each entry a list of strings. Do NOT change the
     name of this function or what it returns.'''
-    # Your code here
+    presenters = {}
+    if(year == "2013" or year == "2015"):
+        official_awards = OFFICIAL_AWARDS_1315
+    else:
+        official_awards = OFFICIAL_AWARDS_1819
+
+    award_time_dict = {}
+    # punctuation fucked shit up
+    for award in official_awards:
+        for ch in string.punctuation:
+            award = award.replace(ch, "")
+        award_tokens = [t.lower() for t in award.split() if t.lower() not in stop_words]
+        if 'television' in award_tokens:
+            award_tokens[award_tokens.index('television')] = 'tv'
+
+        if 'tv' in award_tokens:
+            if 'motion' in award_tokens:
+                award_tokens.remove('motion')
+            if 'picture' in award_tokens:
+                award_tokens.remove('picture')
+
+        # bgms = []
+
+        possible_presenters = {}
+        # check to see if tweet has words in award name
+        # remove award words and stop words
+        timestamp_counter = 0
+        timestamp_sum = 0
+        for tweet in award_tweets:
+            tweet_tokens = tweet["text"]
+
+            combined_tokens = [value for value in award_tokens if value in tweet_tokens]
+            percent = float(len(combined_tokens) / len(award_tokens))
+            if percent > .9:
+                timestamp_counter += 1
+                timestamp_sum += tweet["timestamp_ms"]
+                #presenter_name = [word for word in tweet_tokens if word not in award_tokens]
+                # bgms.extend(nltk.bigrams(presenter_name))
+
+        award_avg = float(timestamp_sum / timestamp_counter)
+        award_time_dict[award] = award_avg
+
+        # freq = nltk.FreqDist(bgms)
+        # presenters[award] = sorted(freq, key=freq.get, reverse=True)[:1]
+
+    for key, value in award_time_dict.items():
+        bgms = []
+        for tweet in all_tweets:
+            if value - 300000 < tweet["timestamp_ms"] < value:
+                bgms.extend(nltk.bigrams(tweet["text"]))
+        freq = nltk.FreqDist(bgms)
+        presenters[key] = sorted(freq, key=freq.get, reverse=True)[:3]
     return presenters
 
 
 host_tweets = []
 award_tweets = []
 presenter_tweets = []
+all_tweets = []
 
 
 def pre_ceremony():
@@ -154,6 +206,7 @@ def pre_ceremony():
     Do NOT change the name of this function or what it returns.'''
     # Your code here
     host_terms = ['host', 'hosts', 'hosting', 'cohosts', 'cohosting', 'cohost']
+    presenter_terms = ['presenting', 'presented', 'presenter', 'presents']
     with open('gg2013.json') as f:
         data = json.load(f)
     for t in data:
@@ -164,11 +217,13 @@ def pre_ceremony():
             tokens = [t.lower() for t in tweet.split() if t.lower() not in stop_words]
             # print (tokens)
             t["text"] = tokens
+            all_tweets.append(t)
             if any(w in host_terms for w in tokens):
                 host_tweets.append(t)
             if "best" in tokens or "award" in tokens:
                 award_tweets.append(t)
-
+            if any(w in presenter_terms for w in tokens):
+                presenter_tweets.append(t)
     print("Pre-ceremony processing complete.")
     return
 
@@ -177,8 +232,9 @@ def main():
     pre_ceremony()
     # print (get_hosts(host_tweets))
     # print (get_awards("2013"))
-    winners = (get_winner("2013"))
-    for keys, values in winners.items():
+    # winners = (get_winner("2013"))
+    presenters = (get_presenters("2013"))
+    for keys, values in presenters.items():
         print(keys)
         print(values)
     return
@@ -186,13 +242,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# ['tv', 'series', 'comedy', 'musical']
-# ['actress', 'tv', 'mucial', 'comedy']
-# ['actor', 'tv', 'musical', 'comedy']
-# ['tv', 'mini', 'series', 'picture']
-# ['actress', 'mini', 'picture', 'tv']
-# ['actor', 'mini', 'picture', 'tv']
-# ['actress', 'supporting', 'tv', 'series']
-# ['actor', 'supporting', 'tv', 'series']
