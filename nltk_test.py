01452fc36cdb7eaf107b290d0f953543d8c6d3db
acbosset <andrewbosset2019@u.northwestern.edu>
2019-02-17 20:41:11 -0600

    adds person name finder, changes approach to presenters

--- nltk_test.py
+++ nltk_test.py
@@ -4,20 +4,22 @@ from nltk.corpus import stopwords
 import string
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from imdb import IMDb
+from collections import Counter
+import re
 # filter = "JSON file (*.json)|*.json|All Files (*.*)|*.*||"
 # filename = rs.OpenFileName("Open JSON File", filter)
 # data = json.load('gg2013.json')
 
 hosts = []
 i = 0
-
 with open('gg2013.json') as f:
     data = json.load(f)
 
 
 stop_words = (stopwords.words('english'))
 stop_words.extend(['golden', 'globe', 'globes', 'goldenglobes',
-                   'year', 'next', 'made', 'performance', 'role', 'miniseries'])
+                   'year', 'next', 'made', 'performance', 'role', 'miniseries', 'video'])
+stop_words.remove('will')
 
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama',
@@ -138,8 +140,103 @@ def get_winner(year):
 
 def possible_presenters():
     # get list of possible presenters, search through tweets with extra stopwords unti we get a solid list
-    pass
+    possible_presenters = []
+    presenter_terms = ['presenting', 'presented', 'presenter', 'present',
+                       'presents', 'award', 'best', 'picture', 'motion', 'funny', 'president', 'amp', 'cecile', 'standing', 'ovation']
+    for word in OFFICIAL_AWARDS_1315:
+        presenter_terms.append(word)
 
+    for tweet in presenter_tweets:
+        tweet_tokens = tweet["text"]
+        tweet_tokens = [word for word in tweet_tokens if word not in presenter_terms]
+        possible_presenters.extend(nltk.bigrams(tweet_tokens))
+
+    presenters_freq = nltk.FreqDist(possible_presenters)
+
+    # print (len(presenters_freq))
+    possible_presenters = sorted(presenters_freq, key=presenters_freq.get, reverse=True)[:50]
+    # print(possible_presenters)
+    possible_presenters2 = human_names(possible_presenters)
+    #print ((possible_presenters2))
+    # print (possible_presenters)
+    return possible_presenters2
+
+
+def human_names(names):
+    ia = IMDb()
+    tweet_names = []
+
+    name_stop_words = []
+    for name in names:
+        new_name = " ".join(name)
+        # if any(w in new_name for w in name_stop_words) is False:
+        if ia.search_person(new_name):
+            if new_name == ia.search_person(new_name)[0]['name'].lower():
+                tweet_names.append(new_name)
+                print(new_name)
+    return tweet_names
+
+
+# def get_presenters(year):
+#     '''Presenters is a dictionary with the hard coded award
+#     names as keys, and each entry a list of strings. Do NOT change the
+#     name of this function or what it returns.'''
+#     presenters = {}
+#     if(year == "2013" or year == "2015"):
+#         official_awards = OFFICIAL_AWARDS_1315
+#     else:
+#         official_awards = OFFICIAL_AWARDS_1819
+#
+#     award_time_dict = {}
+#     # punctuation fucked shit up
+#     for award in official_awards:
+#         for ch in string.punctuation:
+#             award = award.replace(ch, "")
+#         award_tokens = [t.lower() for t in award.split() if t.lower() not in stop_words]
+#         if 'television' in award_tokens:
+#             award_tokens[award_tokens.index('television')] = 'tv'
+#
+#         if 'tv' in award_tokens:
+#             if 'motion' in award_tokens:
+#                 award_tokens.remove('motion')
+#             if 'picture' in award_tokens:
+#                 award_tokens.remove('picture')
+#
+#         # bgms = []
+#
+#         # check to see if tweet has words in award name
+#         # remove award words and stop words
+#         timestamp_counter = 0
+#         timestamp_sum = 0
+#         for tweet in award_tweets:
+#             tweet_tokens = tweet["text"]
+#
+#             combined_tokens = [value for value in award_tokens if value in tweet_tokens]
+#             percent = float(len(combined_tokens) / len(award_tokens))
+#             if percent > .9:
+#                 timestamp_counter += 1
+#                 timestamp_sum += tweet["timestamp_ms"]
+#                 # presenter_name = [word for word in tweet_tokens if word not in award_tokens]
+#                 # bgms.extend(nltk.bigrams(presenter_name))
+#
+#         award_avg = float(timestamp_sum / timestamp_counter)
+#         award_time_dict[award] = award_avg
+#
+#         # freq = nltk.FreqDist(bgms)
+#         # presenters[award] = sorted(freq, key=freq.get, reverse=True)[:1]
+#
+# # list of names in presenter tweets, check to make sure that bigrams are bigrams in possible presenter
+#     presenter_lst = possible_presenters()
+#     print(presenter_lst)
+#     for key, value in award_time_dict.items():
+#         bgms = []
+#         for tweet in all_tweets:
+#             if value - 200000 < tweet["timestamp_ms"] < value:
+#                 add_bigrams = [bgm for bgm in nltk.bigrams(tweet["text"]) if bgm in presenter_lst]
+#                 bgms.extend(add_bigrams)
+#         freq = nltk.FreqDist(bgms)
+#         presenters[key] = sorted(freq, key=freq.get, reverse=True)[:5]
+#     return presenters
 
 def get_presenters(year):
     '''Presenters is a dictionary with the hard coded award
@@ -151,8 +248,10 @@ def get_presenters(year):
     else:
         official_awards = OFFICIAL_AWARDS_1819
 
-    award_time_dict = {}
-    # punctuation fucked shit up
+    # holds shortened award names
+    short_award_names = {}
+    # keeps track of bigrams of tweets
+    award_bigrams = {}
     for award in official_awards:
         for ch in string.punctuation:
             award = award.replace(ch, "")
@@ -165,46 +264,51 @@ def get_presenters(year):
                 award_tokens.remove('motion')
             if 'picture' in award_tokens:
                 award_tokens.remove('picture')
+        short_award_names[award] = award_tokens
+        award_bigrams[award] = []
 
-        # bgms = []
+    # instead check if one of the names is in tweet, then check what award
+    presenter_lst = possible_presenters()
 
-        # check to see if tweet has words in award name
-        # remove award words and stop words
-        timestamp_counter = 0
-        timestamp_sum = 0
-        for tweet in award_tweets:
-            tweet_tokens = tweet["text"]
-
+    for tweet in award_tweets:
+        tweet_tokens = tweet["text"]
+        tweet = " ".join(tweet_tokens)
+        tweet_bigrams = nltk.bigrams(tweet_tokens)
+        # print('here')
+        # print(presenter_lst)
+        if any(presenter in tweet for presenter in presenter_lst):
+            print("found award and presenter")
+        if any(bgm in tweet_bigrams for bgm in presenter_lst):
+            print("found bigram and presenter")
+        for award_name, award_tokens in short_award_names.items():
             combined_tokens = [value for value in award_tokens if value in tweet_tokens]
             percent = float(len(combined_tokens) / len(award_tokens))
-            if percent > .9:
-                timestamp_counter += 1
-                timestamp_sum += tweet["timestamp_ms"]
-                # presenter_name = [word for word in tweet_tokens if word not in award_tokens]
-                # bgms.extend(nltk.bigrams(presenter_name))
-
-        award_avg = float(timestamp_sum / timestamp_counter)
-        award_time_dict[award] = award_avg
+            if percent > .6:
+                presenter_name = [word for word in tweet_tokens if word not in award_tokens]
+                award_bigrams[award_name].extend(nltk.bigrams(presenter_name))
 
         # freq = nltk.FreqDist(bgms)
         # presenters[award] = sorted(freq, key=freq.get, reverse=True)[:1]
 
-# list of names in presenter tweets, check to make sure that bigrams are bigrams in possible presenter
-    possible_presenters = []
-    for tweet in presenter_tweets:
-        tweet_tokens = tweet["text"]
-        possible_presenters.extend(nltk.bigrams(tweet_tokens))
+    # orders for each award
 
-    presenters_freq = nltk.FreqDist(possible_presenters)
-    possible_presenters = sorted(presenters_freq, key=presenters_freq.get, reverse=True)[:50]
-    print (possible_presenters)
-    for key, value in award_time_dict.items():
-        bgms = []
-        for tweet in all_tweets:
-            if value - 200000 < tweet["timestamp_ms"] < value:
-                bgms.extend(nltk.bigrams(tweet["text"]))
-        freq = nltk.FreqDist(bgms)
-        presenters[key] = sorted(freq, key=freq.get, reverse=True)[:5]
+    for award_name, bigrams in award_bigrams.items():
+        freq = nltk.FreqDist(bigrams)
+        sorted_bgms = sorted(freq, key=freq.get, reverse=True)[:50]
+
+        names = []
+        for b in sorted_bgms:
+            names.append(" ".join(b))
+        #print (names)
+        award_bigrams[award_name] = [name for name in names if name in presenter_lst]
+        #print (award_name)
+        # print(award_bigrams[award_name])
+
+    # for tweet in all_tweets:
+    #     add_bigrams = [bgm for bgm in nltk.bigrams(tweet["text"]) if bgm in presenter_lst]
+    #     bgms.extend(add_bigrams)
+    # freq = nltk.FreqDist(bgms)
+    # presenters[key] = sorted(freq, key=freq.get, reverse=True)[:5]
     return presenters
 
 
@@ -223,7 +327,7 @@ def get_red_carpet(year):
             pass
         bgms.extend(nltk.bigrams(tokens))
     freq = nltk.FreqDist(bgms)
-    for bigram in sorted(freq, key=freq.get, reverse=True)[:100]:
+    for bigram in sorted(freq, key=freq.get, reverse=True)[: 100]:
         name = " ".join(bigram)
         if ia.search_person(name):
             if ia.search_person(name)[0]['name'].lower() == name.lower():
@@ -236,8 +340,8 @@ def get_red_carpet(year):
             if n in text:
                 sentiment = analyzer.polarity_scores(text)
                 names[n] += sentiment['compound']
-    best = sorted(names, key=names.get, reverse=True)[:5]
-    worst = sorted(names, key=names.get)[:5]
+    best = sorted(names, key=names.get, reverse=True)[: 5]
+    worst = sorted(names, key=names.get)[: 5]
     print(best)
     print(worst)
     return
@@ -257,7 +361,7 @@ def pre_ceremony():
     Do NOT change the name of this function or what it returns.'''
     # Your code here
     host_terms = ['host', 'hosts', 'hosting', 'cohosts', 'cohosting', 'cohost']
-    presenter_terms = ['presenting', 'presented', 'presenter', 'presents']
+    presenter_terms = ['presenting', 'presented', 'presenter', 'presents', 'present']
     with open('gg2013.json') as f:
         data = json.load(f)
     for t in data:
@@ -284,17 +388,30 @@ def pre_ceremony():
 
 def main():
     pre_ceremony()
-    get_red_carpet("2013")
-    # print (get_hosts(host_tweets))
+    # get_red_carpet("2013")
+    hosts = (get_hosts(host_tweets))
     # print (get_awards("2013"))
-    # presenters = (get_presenters("2013"))
-    # for keys, values in presenters.items():
-    #     print(keys)
-    #     print(values)
-    winners = (get_winner("2013"))
-    for keys, values in winners.items():
+    presenters = (get_presenters("2013"))
+    for keys, values in presenters.items():
         print(keys)
         print(values)
+    # winners = (get_winner("2013"))
+    #nominees = get_nominees("2013")
+    # for keys, values in winners.items():
+    #     print(keys)
+    #     print(values)
+    # human_names('none')
+    json_output = {}
+    json_output["hosts"] = hosts
+    json_output["award_data"] = {}
+    for award_name in official_awards:
+        json_output["award_data"][award_name] = {}
+        json_output["award_data"][award_name]["nominees"] = nominees[award_name]
+        json_output["award_data"][award_name]["presenters"] = presenters[award_name]
+        json_output["award_data"][award_name]["winner"] = winners[award_name]
+
+    with open('gg.json', 'w') as outfile:
+        json.dump(data, outfile)
     return
 
 
